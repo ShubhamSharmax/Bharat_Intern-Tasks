a8ad4286cc8c75d52249b7bf67fa061b2c46f2c6
Shubham <eca.shubhamsharma@gmail.com>
2023-05-17 16:39:24 +0530

    Added Temprature Converter

---
+++ Temperature_Converter/app.js
@@ -0,0 +1,35 @@
+console.log('app.js included successfully')
+
+function convertTemp() {
+    let input = document.getElementById('inputText');
+    let output = document.getElementById('outputText');
+    let inpSelect = document.getElementById('inpUnit').value;
+    let outSelect = document.getElementById('outUnit').value;
+
+    if (inpSelect === "Calsius") {
+        if (outSelect === "Farenheit") {
+            output.innerText = (input.value * 9 / 5) + 32 + '° F';
+        } else if (outSelect === "Kelvin") {
+            output.innerText = input.value + 273.15 + ' K'
+        } else {
+            output.innerText = input.value + '° C';
+        }
+    }
+    else if (inpSelect === "Farenheit") {
+        if (outSelect === "Calsius") {
+            output.innerText = (input.value - 32) * 5 / 9 + '° C';
+        } else if (outSelect === "Kelvin") {
+            output.innerText = ((input.value - 32) * 5 / 9) + 273.15 + ' K'
+        } else {
+            output.innerText = input.value + '° F';
+        }
+    } else {
+        if (outSelect === "Kelvin") {
+            output.innerText = input.value - 273.15 + '° C';
+        } else if (outSelect === "Kelvin") {
+            output.innerText = input.value + ' K'
+        } else {
+            output.innerText = ((input.value - 273.15) * 9 / 5) + 32 + '° F';
+        }
+    }
+}
