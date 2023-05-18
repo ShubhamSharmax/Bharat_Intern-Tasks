e67a46737fe4ff3f967be74ee3d5557471ccd9a7
Shubham <eca.shubhamsharma@gmail.com>
2023-05-18 18:03:17 +0530

    Fixed spelling mistake

--- Temperature_Converter/app.js
+++ Temperature_Converter/app.js
@@ -4,30 +4,30 @@ function convertTemp() {
     let inpSelect = document.getElementById('inpUnit').value;
     let outSelect = document.getElementById('outUnit').value;
 
-    if (inpSelect === "Calsius") {
+    if (inpSelect === "Celsius") {
         if (outSelect === "Farenheit") {
-            output.innerText = (input.value * 9 / 5) + 32 + '° F';
+            output.innerText = (input.value * 9 / 5) + 32 + ' ° F';
         } else if (outSelect === "Kelvin") {
             output.innerText = input.value + 273.15 + ' K'
         } else {
-            output.innerText = input.value + '° C';
+            output.innerText = input.value + ' ° C';
         }
     }
     else if (inpSelect === "Farenheit") {
-        if (outSelect === "Calsius") {
-            output.innerText = (input.value - 32) * 5 / 9 + '° C';
+        if (outSelect === "Celsius") {
+            output.innerText = (input.value - 32) * 5 / 9 + ' ° C';
         } else if (outSelect === "Kelvin") {
             output.innerText = ((input.value - 32) * 5 / 9) + 273.15 + ' K'
         } else {
-            output.innerText = input.value + '° F';
+            output.innerText = input.value + ' ° F';
         }
     } else {
-        if (outSelect === "Kelvin") {
-            output.innerText = input.value - 273.15 + '° C';
+        if (outSelect === "Celsius") {
+            output.innerText = input.value - 273.15 + ' ° C';
         } else if (outSelect === "Kelvin") {
             output.innerText = input.value + ' K'
         } else {
-            output.innerText = ((input.value - 273.15) * 9 / 5) + 32 + '° F';
+            output.innerText = ((input.value - 273.15) * 9 / 5) + 32 + ' ° F';
         }
     }
 }
